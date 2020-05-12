1a548b09171bc548cc0d9597c093e6fed7c51336
Cesar <cesaregarza>
2020-05-12 02:09:59 -0500

    Added arbitrary mutation chance for crossover function

--- ets_fitness.py
+++ ets_fitness.py
@@ -3,16 +3,40 @@ import genetic
 from numba import jit, double, boolean
 import numpy as np
 import pandas as pd
+import random
+from scipy.optimize import broyden1
+from scipy.special import erf
 
 
+#Crossover with a ~4.5% chance of mutation
 @jit(double(double, double), nopython=True)
 def float_crossover(mom, dad):
     avg = (mom + dad) / 2
     std = abs(mom - dad) / 4
     return np.random.normal(avg, std)
 
+#Crossover with arbitrary chance of mutation. Please note that this will return a function, do not feed it directly.
+def float_crossover_mutation(mutation_chance):
+
+    #Function to optimize
+    def F(x):
+        return erf(x/np.sqrt(2)) + mutation_chance - 1
+    
+    #Find n
+    n = broyden1(F, [1.0])[0]
+
+    #Create the crossover function
+    @jit(double(double, double), nopython=True)
+    def crossover(mom, dad):
+        avg = (mom + dad) / 2
+        std = abs(mom - dad) / (2 * n)
+        return np.random.normal(avg, std)
+    
+    return crossover
+
+
 class ets:
-    def __init__(self, df, max_date = pd.Timestamp.today(), elitism=1, optimizer="genetic hybrid", size=2):
+    def __init__(self, df, max_date = pd.Timestamp.today(), elitism=1, optimizer="genetic hybrid", size=2, mutation_chance=0.045):
         
         if isinstance(max_date, int):
             max_date = df.index[-1]
@@ -34,7 +58,12 @@ class ets:
         # seasonal_lag = self._find_seasonality(self.df)
         # if abs(seasonal_lag[0]) > 0.8:
         #     self.seasonal = True
-        #     self.lag = seasonal_lag[1]    
+        #     self.lag = seasonal_lag[1]
+
+        if mutation_chance != 0.045:
+            crossover = float_crossover_mutation(mutation_chance)
+        else:
+            crossover = float_crossover
 
         min_val, max_val = np.min(self.li), np.max(self.li)
         diff_vals = max_val - min_val
@@ -46,11 +75,11 @@ class ets:
         trend_bounds_m = (-diff_vals, diff_vals, [0])
         # s_list = [trend_bounds] * self.lag
 #                                             alpha         beta            initial_level   initial_trend  phi/gamma   s_list       fitness fxn             mutation          simple crossover          crossover function
-        template1 = genetic.Genetic_Template([alpha_bounds,                 level_bounds],                                          self.fitness_aia,       mutation = False, simple_crossover = False, crossover_function=float_crossover)
-        template2 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds],                          self.fitness_abiaib,    mutation = False, simple_crossover = False, crossover_function=float_crossover)
-        template3 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds, param_bounds],            self.fitness_abiaibd,   mutation = False, simple_crossover = False, crossover_function=float_crossover)
-        template4 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds_m, trend_bounds_m],                        self.fitness_abiaib_m,  mutation = False, simple_crossover = False, crossover_function=float_crossover)
-        # template5 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds, param_bounds, *s_list],   self.fitness_abiaibgs,  mutation = False, simple_crossover = False, crossover_function=float_crossover)
+        template1 = genetic.Genetic_Template([alpha_bounds,                 level_bounds],                                          self.fitness_aia,       mutation = False, simple_crossover = False, crossover_function=crossover)
+        template2 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds],                          self.fitness_abiaib,    mutation = False, simple_crossover = False, crossover_function=crossover)
+        template3 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds, param_bounds],            self.fitness_abiaibd,   mutation = False, simple_crossover = False, crossover_function=crossover)
+        template4 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds_m, trend_bounds_m],                        self.fitness_abiaib_m,  mutation = False, simple_crossover = False, crossover_function=crossover)
+        # template5 = genetic.Genetic_Template([alpha_bounds, param_bounds,   level_bounds,   trend_bounds, param_bounds, *s_list],   self.fitness_abiaibgs,  mutation = False, simple_crossover = False, crossover_function=crossover)
 
         pool1 = genetic.Genetic_Pool(template1, elitism=elitism)
         pool2 = genetic.Genetic_Pool(template2, elitism=elitism)
